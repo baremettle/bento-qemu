f2ba9b97d3802c837c38ed3ec1d25240de42ad7f
Brian Clark <bclarkindy@users.noreply.github.com>
2014-06-03 23:33:33 -0400

    Update packer.rb to handle STDIN (untested)

--- lib/bento_qemu/packer.rb
+++ lib/bento_qemu/packer.rb
@@ -7,11 +7,13 @@ module BentoQemu
       def build(template, args = {})
         packer_options = args.fetch(:packer_options, [])
         shell_cwd = args.fetch(:cwd, Dir.pwd)
+        std_in = args.fetch(:input, nil)
         cmd = [%(packer build)]
         cmd << packer_options
-        cmd << template unless args[:input]
+        cmd << template unless std_in
         shellout = Mixlib::ShellOut.new(cmd.join(' '), :timeout => 3600)
         shellout.cwd = shell_cwd
+        shellout.input = std_in
         shellout.live_stream = STDOUT
         shellout.run_command
         shellout.error!
