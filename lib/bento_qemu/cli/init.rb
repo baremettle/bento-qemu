745e41fa2aaa20180f39e8be603e1755a0569a05
Brian Clark <brian@clark.zone>
2014-06-03 22:26:22 -0400

    get rid of artifact_dir and packer_dir

--- lib/bento_qemu/cli/init.rb
+++ lib/bento_qemu/cli/init.rb
@@ -11,19 +11,12 @@ module BentoQemu
     method_option(:bento_ref,
                   :default => 'master',
                   :desc => 'branch/tag/ref to download from bento')
-    method_option(:artifact_dir,
-                  :default => '../artifacts',
-                  :desc => 'directory to store packer artifacts' \
-                    ' (relative to template file!!)')
     method_option(:build_dir,
                   :default => './builds',
                   :desc => 'where to store builds from box conversions')
     method_option(:bento_dir,
                   :default => './bento',
                   :desc => 'directory name of downloaded bento repository')
-    method_option(:packer_dir,
-                  :default => './packer_qemu',
-                  :desc => 'directory to store converted templates')
     def init
       require 'yaml'
       File.open('.bento_qemu.yml', 'w') { |f| f.write options.to_hash.to_yaml }
